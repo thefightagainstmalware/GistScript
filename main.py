494adeac160ec27dd269ef419606d5daa779cd5c
PandaNinjas <admin@malwarefight.gq>
2022-04-05 10:53:46 -0700

    Added support for base64 encoded webhook

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import requests, re, datetime, time
+import requests, re, time, base64
 
 DISCORD_WEBHOOK_REGEX = re.compile(
     r"(https?:\/\/(ptb\.|canary\.)?discord(app)?\.com\/api\/webhooks\/(\d{18})\/([\w\-]{68}))"
 )
+
+BASE64_REGEX = re.compile("^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?")
 
 with open("gists.txt") as f:
     PASTEBINS = f.read().splitlines()
@@ -28,12 +30,23 @@ def delete_webhook(webhook: str) -> None:
     print(resp)
     print(requests.delete(webhook))
 
+
 while True:
     for pastebin in PASTEBINS:
-        resp = requests.get(pastebin, headers={"User-Agent": "AntiMalwareBot/gistscript (+https://discord.gg/TWhrmZFXqb)"})
+        resp = requests.get(
+            pastebin,
+            headers={
+                "User-Agent": "AntiMalwareBot/gistscript (+https://discord.gg/TWhrmZFXqb)"
+            },
+        )
         if resp.status_code != 200:
             print("Error: " + str(resp.status_code))
             continue
-        for webhook in DISCORD_WEBHOOK_REGEX.findall(resp.text):
+
+        text = resp.text
+        while BASE64_REGEX.match(text) is not None:  # screw you, massileQOL
+            text = base64.b64decode(text)
+
+        for webhook in DISCORD_WEBHOOK_REGEX.findall(text):
             delete_webhook(webhook[0])
     time.sleep(5)
